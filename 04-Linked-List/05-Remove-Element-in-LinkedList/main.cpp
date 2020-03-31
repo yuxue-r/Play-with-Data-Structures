013f8a378247885f8c5c2d08d72aeff1be537378
yyz000 <41008530+yyz000-yuxue@users.noreply.github.com>
2020-03-31 19:59:26 +0800

    add code

---
+++ 04-Linked-List/05-Remove-Element-in-LinkedList/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "LinkList.h"
+using  namespace std;
+
+
+void demo1();
+int main()
+{
+    demo1();
+
+    return 0;
+}
+void demo1()
+{
+    LinkList<int> linkList =  LinkList<int>();
+    for ( int i = 0; i < 5; ++i )
+    {
+        linkList.addFirst(i);
+        linkList.print();
+    }
+    linkList.add(2,666);
+    linkList.addLast(999);
+    linkList.print();
+//    cout<<linkList.get(2)<<endl;
+//    cout<<linkList.contains(999)<<endl;
+    linkList.remove(2);
+    linkList.print();
+    linkList.removeFirst();
+    linkList.print();
+    linkList.removeLast();
+    linkList.print();
+
+    linkList.removeElements(0);
+    linkList.print();
+}
