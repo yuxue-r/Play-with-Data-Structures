d29b69399a7cc98887eb1345b4c9a6d7e0565847
yyz000 <41008530+yyz000-yuxue@users.noreply.github.com>
2020-04-05 21:29:05 +0800

    add code

---
+++ 06-Binary-Search-Tree/06-PreOrder-Traverse-in-BST/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include "BST.h"
+
+using namespace std;
+
+void demo1();
+void demo2();
+
+int main()
+{
+    demo2();
+    return 0;
+}
+void demo2()
+{
+    BST<int> bst = BST<int>();
+    int nums[] = {5, 3, 6, 8, 4, 2};
+    for (int num : nums)
+    {
+        bst.add(num);
+    }
+    bst.preOrder();
+    bst.print();
+}
+
+void demo1()
+{
+    BST<int> bst = BST<int>();
+    bst.add(1);
+    bst.add(5);
+    bst.add(10);
+    bst.add(6);
+
+
+    cout<<bst.contain(1)<<endl;
+    cout<<bst.contain(5)<<endl;
+    cout<<bst.contain(10)<<endl;
+
+
+    cout<<bst.getSize()<<endl;
+}
